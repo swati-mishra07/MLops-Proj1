bbb828d4cf7378190f7d9287f2ae1e27afe82ca7
swati-mishra07 <Aanamishra2407@gmail.com>
2026-09-15 16:25:40 +0530

    Project Structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Swati Mishra",
+    author_email="aanamishra2407@gmail.com",
+    packages=find_packages(),
+)
